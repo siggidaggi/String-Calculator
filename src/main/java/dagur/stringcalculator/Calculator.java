b3bd8e757c87963a6f91c9c77526f2b9b32f64c5
Sigurður Dagur Sturluson <sigurdurds14@ru.is>
2016-10-13 12:33:31 +0000

    beinagrind af verkefni

---
+++ src/main/java/dagur/stringcalculator/Calculator.java
@@ -0,0 +1,14 @@
+package dagur.stringcalculator;
+
+public class Calculator {
+
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else
+			return 1;
+	}
